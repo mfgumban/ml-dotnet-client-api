78a7946d4b93b59b37c870786f5ca82647dac483
Mark Fredrik Gumban <mark.gumban@marklogic.com>
2019-12-02 22:59:23 +0800

    decorator base tests

---
+++ MarkLogic.Client.Tests/DataServices/DecoratorBaseService.cs
@@ -0,0 +1,32 @@
+﻿using MarkLogic.Client.DataService;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace MarkLogic.Client.Tests.DataServices
+{
+    public class DecoratorBaseService : DataServiceBase
+    {
+        protected DecoratorBaseService(IDatabaseClient dbClient) : base(dbClient, "/test/decoratorBase/")
+        {
+        }
+
+        public static DecoratorBaseService Create(IDatabaseClient dbClient)
+        {
+            return new DecoratorBaseService(dbClient);
+        }
+
+        public ISessionState NewSession()
+        {
+            return DbClient.CreateSession();
+        }
+
+        public Task<JObject> Docify(string value)
+        {
+            return CreateRequest("docify.sjs")
+                .WithParameters(
+                    new SingleParameter<string>("value", false, value, Marshal.String)
+                )
+                .RequestSingle<JObject>(true, Unmarshal.JsonObject);
+        }
+    }
+}
---
+++ MarkLogic.Client.Tests/DataServices/DecoratorBaseTests.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MarkLogic.Client.Tests.DataServices
+{
+    public class DecoratorBaseTests : DbTestBase, IClassFixture<DatabaseClientFixture>
+    {
+        public DecoratorBaseTests(DatabaseClientFixture dbClientFixture, ITestOutputHelper output)
+            : base(dbClientFixture, output)
+        {
+        }
+
+        [Fact]
+        public async void Docify()
+        {
+            const string input = "value1";
+            var dbNode = await DecoratorBaseService.Create(DbClient).Docify(input);
+            Assert.NotNull(dbNode);
+
+            JToken dbValue = null;
+            var success = dbNode.TryGetValue("value", out dbValue);
+            Assert.True(success);
+            Assert.NotNull(dbValue);
+            Assert.Equal(JTokenType.String, dbValue.Type);
+            Assert.Equal(input, dbValue.ToString());
+        }
+    }
+}
